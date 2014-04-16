ba29adb322ccb41acb4045ed6f345ec64114ba65
mgodoy <mgodoy@despegar.com>
2014-04-16 14:17:34 -0300

    Catch exception

--- WTFPlugin/src/wtfplugin/statusbar/WTFStatusBar.java
+++ WTFPlugin/src/wtfplugin/statusbar/WTFStatusBar.java
@@ -2,6 +2,7 @@ package wtfplugin.statusbar;
 
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.eclipse.core.resources.IFile;
@@ -12,7 +13,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.MouseAdapter;
@@ -114,8 +114,7 @@ public class WTFStatusBar extends WorkbenchWindowControlContribution {
 							}
 						}
 				} catch (CoreException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					Activator.showException(e1);
 				}
 			}
 		}
@@ -131,20 +130,27 @@ public class WTFStatusBar extends WorkbenchWindowControlContribution {
 		
 		@Override
 		public void mouseUp(MouseEvent e) {
+			InputStream is = null;
 			try {
 				IFile file = project.getFile(".wtf");
 				Properties props = new Properties();
-				props.load(file.getContents());
+				is = file.getContents();
+				props.load(is);
 				LaunchTomcat.launchTomcat(project, props.getProperty("contextPath", "/" + project.getName()), props.getProperty("port", "8080"), props.getProperty("serverport", "8208"), props.getProperty("contextcontent",""), props.getProperty("jvmargs",""));
 			} catch (JavaModelException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				Activator.showException(e1);
 			} catch (CoreException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				Activator.showException(e1);
 			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				Activator.showException(e1);
+			} finally {
+				if (is!= null) {
+					try {
+						is.close();
+					} catch (IOException e1) {
+						Activator.showException(e1);
+					}
+				}
 			}
 		}
 		
